deaf26a5ffa4f2b0375591e1f68a2d79a2bf7384
alexv710 <alexander.wyss@uzh.ch>
2023-07-29 14:24:30 +0200

    refactor(typescript):update the interface for the patient with an optional id and some other missing attributes
    new patient don't yet have an id

--- types/Patient.ts
+++ types/Patient.ts
@@ -1,18 +1,15 @@
-export interface Address {
+export interface Patient {
+  id?: string
+  firstname: string
+  lastname: string
+  gender: string
+  email: string
+  phone: string
+  ahvNumber: string
+  dateOfBirth: string | null
   streetName: string
   streetNumber: string
   postalCode: string
   city: string
   canton: string
-}
-
-export interface Patient {
-  firstname: string
-  lastname: string
-  ahvNumber: string
-  dateOfBirth: string | null
-  gender: string
-  phone: string
-  email: string
-  address: Address
 }
